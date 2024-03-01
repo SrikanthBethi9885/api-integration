a50cd05b65325556a4f6cfc827dd5215188b6835
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2024-03-01 12:33:11 +0530

    csv to json file

--- JsonPlaceholder/csvtojson.js
+++ JsonPlaceholder/csvtojson.js
@@ -4,20 +4,23 @@ const filepath='./customers-100.csv'
 
 async function get(){
     const customersjson=await csv().fromFile(filepath);
-    //console.log(customersjson)
+    console.log(customersjson)
     let company=[];
     let country=[];
     let website=[];
+    let email = [];
     for(let index=0;index<customersjson.length;index++){
         let element=customersjson[index]
         company.push(element.Company);
         country.push(element.Country);
-        website.push(element.Website)
+        website.push(element.Website);
+        email.push(element.Email);
     }
     console.log(country.length)
     const uniqueCountries = [...new Set(country)];
     console.log('uniqueCountries:', uniqueCountries.length);
     fs.writeFileSync('country.json',JSON.stringify(uniqueCountries,null,2));
-    fs.writeFileSync('website.json',JSON.stringify(website,null,2))
+    fs.writeFileSync('website.json', JSON.stringify(website, null, 2));
+    fs.writeFileSync('email.json', JSON.stringify(email, null, 2))
 }
 get()
